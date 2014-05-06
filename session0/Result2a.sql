b28e9c29b9b7e21be4729b9b7a453eb3c90bcbdb
basUT <basj95@hotmail.com>
2014-05-06 15:32:23 +0200

    Exercise 2 FULL

---
+++ session0/Result2a.sql
@@ -0,0 +1,5 @@
+INSERT INTO F(x,y,violation,motivation)
+	VALUES(ABC, EF, false, "vanuit ABC kan je naar EF komen, dus heb je al ABCEF. Vanuit E kom je bij D (ABCDEF) en vanuit D kom je weer bij A."),
+		(E,D,true, "vanuit E kom je bij D, dan heb je ED. Vanuit D kom je bij A, dan heb je ADE. Vanuit ADE kun je geen ABC vormen om EF te krijgen."),
+		(D,A,true, "vanuit D kom je in A, maar vanuit AD kan je niks meer.");
+		
